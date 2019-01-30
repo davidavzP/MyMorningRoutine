bb086d4c9ea8b21488468606868e35bbf06437f6
David Pojunas <davidapojunas@gmail.com>
2019-01-29 23:10:05 -0600

    Added Titles

--- app/src/main/java/com/example/mymorningroutine/MainActivity.java
+++ app/src/main/java/com/example/mymorningroutine/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.mymorningroutine;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,16 +16,34 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
 
+    private TextView textView;
+    private TextView timeText;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = findViewById(R.id.textView);
+        textView.setText("THE DEADLINE!");
+        timeText = findViewById(R.id.textTime);
+        timeText.setText("TIME REMAINING");
+
+
+
+
+
+
+
+
         listView = (ListView)findViewById(R.id.listView);
 
         final ArrayList<String> test = new ArrayList<>();
 
-        for(int i = 0; i< 15; i++){
+        for(int i = 0; i< 25; i++){
             test.add("Task that needs to be done-" + i);
         }
 
